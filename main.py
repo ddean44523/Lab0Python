ec87865c3b858c38cf5aebc006d8f158e16fdc36
DidierOne <dpd5518@psu.edu>
2020-08-25 15:36:43 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+userName = input("Enter your name:")
+print("Hello," + userName + "!")
+
+userAge = input("Enter your age:")
+userAge = int(userAge)
+print("you will be " + str(userAge + 1) +" next year!")
+
